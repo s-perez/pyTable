3f5595d1fbccdbbdd08ae11b1c393cf26806a7ae
kuraime <sisekeom@gmail.com>
2013-01-16 16:11:48 +0100

    Initial version

---
+++ SymTable.py
@@ -0,0 +1,60 @@
+from Table import Table
+
+
+class SymTable():
+
+    def __init__(self):
+        self.tables = []
+        self.nextId = 0
+
+    def newTable(self):
+        "Creates a new symbol table and returns its ID"
+        a = Table(self.nextId)
+        self.tables.append(a)
+        self.nextId = self.nextId + 1
+        return self.nextId - 1
+
+    def destroyTable(self, id):
+        "Deletes table ID"
+        self.tables[id].delete()
+        pass
+
+    def existTable(self, id):
+        "Checks wether Table ID exists or not"
+        a = self.tables[id]
+        if not a or not a.exist():
+            return False
+        else:
+            return True
+
+    def add(self, id, lex):
+        """Adds the lexem lex to table ID.
+        Returns the ID of the lex if everything is OK or false if lex already exist on that table"""
+        return self.tables[id].add(lex)
+
+    def addType(self, id, lex, type):
+        """Sets the type of lex in table id to type.
+        Returns True if everything is OK and false if lex does not exist on that table"""
+
+        return self.tables[id].setType(lex, type)
+
+    def checkType(self, id, lex):
+        """Returns the type of lex on table id"""
+
+        return self.tables[id].getType(lex)
+
+    def existsEntry(self, id, lex):
+        """checks wether lex exists on table id"""
+        return self.tables[id].contains(lex)
+
+    def writeTable(self, path):
+        """This function writes the content of all the tables into file pointed by path"""
+
+        for tab in self.tables:
+            tab.write(path)
+        return True
+
+    def searchPos(self, id, lex):
+        """Returns the id of lex into table id"""
+
+        return self.tables[id].getPos(lex)
---
+++ Table.py
@@ -0,0 +1,76 @@
+class Table():
+
+    def __init__(self, id):
+        self.lexems = {}
+        self.exists = True
+        self.id = id
+
+    def delete(self):
+        """This function marks this table as deleted"""
+
+        self.exists = False
+        return True
+
+    def exist(self):
+        """Checks if this table is deleted or not"""
+
+        return self.exists
+
+    def add(self, lex):
+        """Adds lex to this table.
+        Returns the position of the lex if everything went OK. Otherwise, it returns False"""
+
+        if not lex in self.lexems:
+            self.lexems[lex] = ''
+            return self.getPos(lex)
+        else:
+            return False
+
+    def setType(self, lex, type):
+        """Sets the type of lex.
+        Returns True if everything went OK. Otherwise, returns False"""
+
+        if lex in self.lexems:
+            self.lexems[lex] = type
+            return True
+        else:
+            return False
+
+    def getType(self, lex):
+        "Returns the type of lex"
+
+        return self.lexems[lex]
+
+    def contains(self, lex):
+        "Checks if lex is in the table or not"
+
+        return self.lexems.has_key[lex]
+
+    def write(self, path):
+        "Writes the contents of this table to the file pointed to by path"
+
+        if self.exist():
+            f = open(path, 'a')
+            f.write('--------------------| Tabla' + str(self.id) + ' |--------------------\n')
+            f.write('----------------------------------------------------\n')
+            for e in self.lexems.keys():
+                f.write('\t' + str(e))
+                if self.lexems[e] != '':
+                    f.write(' (' + self.lexems[e] + ')')
+                f.write('\n')
+            f.write('\n\n\n')
+            f.close()
+            return True
+        else:
+            return False
+
+    def getPos(self, lex):
+        "returns the ID of lex or False if lex is not in this table"
+
+        i = 0
+        for e in self.lexems.keys():
+            if e == lex:
+                break
+            else:
+                i = i + 1
+        return i if i == len(self.lexems) else False
